c6a14650f93659c4218cf8c1cc5ae223e43bee23
Jeremy <jeremy@contextoptional.com>
2012-01-06 11:47:19 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gobbs::Application.config.secret_token = 'aa9589d145afb47a2b0056c032d70a7d276622e68312030c005e6173993235a845818e3e3fd8b0f051fb7b345f94413f3d895425ed5f63267de94b96b97222ed'
